8da41751bff5e0ad0ca3c16b0263edb210883154
merhawit <merhawit54tad@gmail.com>
2022-09-12 14:52:59 -0700

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the           cat >6-size.c
+#include <stdio.h>
+
+
+/** 
+* main - print the string in the put function
+*
+*Description: using the main function 
+*this program prints "programing is like buliding a multilingual puzzle 
+*return:0
+*/
+int main(void)
+{
+           char c;
+           int i;
+           long li;
+           long long lli;
+           flost f;
+
+           printf("size of a char: %1d byte(s)\n", sizeof(c));
+           printf("size of an int: %1d byte(s)\n", sizeof(i));
+           printf("size of a long int: %1d byte (s)\n" , sizeof(li));
+           printf("size of a long long int: 51d byte (s)\n, sizeo(llo));
+           printf("size of a float: %1d byte(s)\n", sizeof(f));
+           return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+/**
+* main - print the string in the put function 
+*
+*Description: using the main function
+*this program prints "programing is like buliding a multilingual puzzle*this program prints "programing i*return:0
+*/
+int main(void) 
+{
+              char c;
+              int i;
+             long li;
+
+              long long lli; 
+              float f;
+
+              printf("size of a char: %1d byte(s)\n", sizeof(c));
+              printf("size of an int: %1d byte(s)\n", sizeof(i));
+              printf("size of a long int: %1d byte (s)\n" , sizeof(li));
+              printf("size of a long long int: 51d byte (s)\n, sizeo(llo));
+              printf("size of a float: %1d byte(s)\n", sizeof(f));
+
+              return (0);
+}
