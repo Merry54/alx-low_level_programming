4a863d79d1d138610620f9fb19fa0680a4127752
merhawit <merhawit54tad@gmail.com>
2022-09-12 14:59:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,29 +1,14 @@
+
 #include <stdio.h>
 
 /**
-* main - print the string in the           cat >6-size.c
-#include <stdio.h>
-
-
-/** 
-* main - print the string in the put function
+*main - print the string in the put function  
 *
-*Description: using the main function 
-*this program prints "programing is like buliding a multilingual puzzle 
-*return:0
+*this program prints "Programming is like building a multilingual puzzle  
+*return :0   
 */
-int main(void)
+int main (void)
 {
-           char c;
-           int i;
-           long li;
-           long long lli;
-           flost f;
-
-           printf("size of a char: %1d byte(s)\n", sizeof(c));
-           printf("size of an int: %1d byte(s)\n", sizeof(i));
-           printf("size of a long int: %1d byte (s)\n" , sizeof(li));
-           printf("size of a long long int: 51d byte (s)\n, sizeo(llo));
-           printf("size of a float: %1d byte(s)\n", sizeof(f));
-           return (0);
+             puts ("\'Programming is like building a multilingual puzzle");
+             return (0);
 }
