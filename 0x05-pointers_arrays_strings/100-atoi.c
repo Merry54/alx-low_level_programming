33d5ce7ac7f97f6181c4f6a956e9ab2b57c7dc07
merhawit <merhawit54tad@gmail.com>
2022-09-19 14:41:02 -0700

    changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,20 +3,37 @@
 /**
 * _atoi - converts a string to an int
 * @s: the string to be change
-*
 * Return: the converted int
 */
 
 int _atoi(char *s)
+
 {
-int sign = 1;
-unsigned int num = 0;
-do {
-if (*s == '-')
-sign *= -1;
-else if (*s >= '0' && *s <= '9')
-num = num * 10 + (*s - '0');
-else if (num > 0)															break;
-} while (*s++);
-return (num * sign);
+
+		int sign = 1;
+
+			unsigned int num = 0;
+
+
+
+				do {
+
+							if (*s == '-')
+
+											sign *= -1;
+
+									else if (*s >= '0' && *s <= '9')
+
+													num = num * 10 + (*s - '0');
+
+											else if (num > 0)
+
+															break;
+
+												} while (*s++);
+
+
+
+					return (num * sign);
+
 }
