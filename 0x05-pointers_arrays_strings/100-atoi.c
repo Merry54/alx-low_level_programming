e44bcfad269d504217c71f8470f8515cc90288bd
merhawit <merhawit54tad@gmail.com>
2022-09-19 14:54:10 -0700

    changes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,12 +12,14 @@ int sign = 1;
 
 unsigned int num = 0;
 do {
+
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
-num = num * 10 + (*s - '0')
-else if (num >0)
+num = num * 10 + (*s - '0');
+else if (num > 0)
 break;
+
 } while (*s++);
 return (num * sign);
 }
