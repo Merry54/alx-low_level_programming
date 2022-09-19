85a389db918580d8fb4fe7abd38b72b75656311d
merhawit <merhawit54tad@gmail.com>
2022-09-19 14:23:56 -0700

    changes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,17 @@
 void rev_string(char *s)
 {
 
-	int len, i, half;
-	char temp;
+int len, i, half;
+char temp;
 
-	for (len = 0; s[len] != '\0'; len++)
-		;
-
-	i = 0;
-
-	half = len / 2;
-
-	while (half--)
-	{
-			temp = s[len - i - 1];
-				s[len - i - 1] = s[i];
-					s[i] = temp;
-						i++;
-	}
-
+for (len = 0; s[len] != '\0'; len++);
+i = 0;
+half = len / 2;
+while (half--)
+{
+temp = s[len - i - 1];
+s[len - i - 1] = s[i];
+s[i] = temp;
+i++;
 }
-
+}
