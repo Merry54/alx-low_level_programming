962366b7a96ca7e54d8b9890b85d7c9c282a0afc
merhawit <merhawit54tad@gmail.com>
2022-09-21 11:11:51 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H                                                                                                                          
+#define MAIN_H                                                                                                                          
+#include<stdio.h>                                                                                                                       
+
+/**                                                                                                                                     
+ * void prototype()                                                                                                                  
+ * int prototypes()                                                                                                                 
+ * char protoypes                                                                                                                  
+ */                                                                                                                                     
+void reverse_array(int*a,int n);                                                           
+
+void print_number(int n)                                                                                                                
+	void print_buffer(char*b,int size0;                                                                                            
+int_strcmp(char*s1,char*src);                                                                                  
+char*_strcat(char*dest,char*stc);                                                                              
+char*_strcat(char*dest,char*char*src,int n);                                                                   
+char*_string_toupper(char*);                                                                                    
+char*strncpy(char*);                                    
+char*cap_string(char);                                                                                         
+char*leet(char*);
+char*rot13(char*)
+char*infinite_add(char*n1,char*n2,char*r,int size_r);
+char*add_add_strings(char*n1,char*n2,char*r,int r_index);
+
+#endif /*MAIN_H/
