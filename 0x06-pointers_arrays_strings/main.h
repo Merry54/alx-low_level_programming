fcb50c270eb9624cfdc981df4f302e5f19f14930
merhawit <merhawit54tad@gmail.com>
2022-09-22 15:21:48 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,21 +1,24 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *p;
+*void prototypes()
+*int prototype ()
+*char prototypes()
+*/
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    p = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", p);
-    return (0);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char*b,int size)
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
