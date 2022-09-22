c3c5a276aaa4004da312b7af3bfb16161de27f37
merhawit <merhawit54tad@gmail.com>
2022-09-22 12:14:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,25 +1,19 @@
-#ifndef MAIN_H                                                                                                                          
-#define MAIN_H                                                                                                                          
-#include<stdio.h>                                                                                                                       
-
-/**                                                                                                                                     
- * void prototype()                                                                                                                  
- * int prototypes()                                                                                                                 
- * char protoypes                                                                                                                  
+#ifndef MAIN_H                                                                  #define MAIN_H                                              
+#include<stdio.h>                                                                                                                  
+/**                                                                  
+ * void prototype()                                                              * int prototypes()                                                              * char protoypes                                                            
  */                                                                                                                                     
-void reverse_array(int*a,int n);                                                           
-
-void print_number(int n)                                                                                                                
-	void print_buffer(char*b,int size0;                                                                                            
-int_strcmp(char*s1,char*src);                                                                                  
-char*_strcat(char*dest,char*stc);                                                                              
-char*_strcat(char*dest,char*char*src,int n);                                                                   
-char*_string_toupper(char*);                                                                                    
-char*strncpy(char*);                                    
-char*cap_string(char);                                                                                         
-char*leet(char*);
-char*rot13(char*)
-char*infinite_add(char*n1,char*n2,char*r,int size_r);
-char*add_add_strings(char*n1,char*n2,char*r,int r_index);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /*MAIN_H/
