eccf645cf5b703d2b23ee4ed26403ab2a0950dc5
merhawit <merhawit54tad@gmail.com>
2022-09-22 14:48:59 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 #include <stdio.h>
 
+/**
+ * void prototype()
+ * int prototype()
+ * char prototype()
+ */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
