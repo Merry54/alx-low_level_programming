005ca7a57224bb991ae1d35edb897d0039d57446
merhawit <merhawit54tad@gmail.com>
2022-09-22 16:27:48 -0700

    zero

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,6 @@
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char*b,int size)
-int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -21,5 +20,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, chat *r, int r_index);
 
 #endif/*MAIN_H*/
