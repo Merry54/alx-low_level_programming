1008952a83e293025765efd14ed5925ff60e5a45
merhawit <merhawit54tad@gmail.com>
2022-09-28 09:41:38 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recusion - prints's a strinng followed by a new line 
+ * @s: string to beb printed 
+ *
+ * return:void 
+ */
+
+void _puts_recusion(chaar *s)
+{
+	if (*s =='\0')
+	{
+		_putchar('\n');
+   }
+	else 
+ {
+	_puchar(s[0]);
+	_puts_recusion(s+1);
+}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts _recursion(char *s);
+int _prints_rev_recusion(char *s)
+int _strlen_recusion(char *s);
+int factorial (int n);
+int _pow_recusion(int x, int y);
+int _sqrt_recusion(int n)
+int is_prime_number(int n);
+int _sqrt(recusion(int n);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag)
+int wildcmp(char *s1, char*s2);
+
+#endif /MAIN_H/
+
