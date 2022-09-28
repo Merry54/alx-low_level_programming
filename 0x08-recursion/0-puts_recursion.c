568fec8ad05185bd37e24b574ed91daa6fdf4c47
merhawit <merhawit54tad@gmail.com>
2022-09-28 12:09:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _puts_recusion - prints's a strinng followed by a new line 
+* _puts_recusion - prints's a string followed by a new line 
 * @s: string to beb printed 
 *
 * return:void 
@@ -9,13 +9,13 @@
 
 void _puts_recusion(char *s)
 {
-if (!s || !*s)
+if (*s =='\0')
 {
 _putchar('\n');
-returns ;
 }
+else
 {
-_puchar(*s);
+_puchar(s[0]);
 _puts_recusion(s+1);
 }
 }
