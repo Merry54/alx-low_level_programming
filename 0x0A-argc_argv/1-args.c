1d6fb2d92f74007b1694d27053a517e8a19eb5a8
merhawit <merhawit54tad@gmail.com>
2022-09-30 04:08:23 -0700

    all

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- the program should print a number, followed by a new line
+ * @argc: int  
+ * @argv: list
+ *return: 0
+ */
+
+int main (int argc, char const *argv[])
+{
+(void)argv;
+printf(%i\n, argc -1);
+return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * this program prints all arguments, including the first line and only print one argumentper line,ending with a new line
+ * main:
+ *
+ * return:0
+ */
+
+int(int argc, char argv[])
+{
+int i =0;
+
+while (argc--)
+{
+printf("%s\n", argv=[1]);
+i++;
+}
+return (0);
+}
