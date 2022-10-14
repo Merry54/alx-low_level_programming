2fd8ddd5d2428a8fbe4ed838fb9d31560ab236f7
merhawit <merhawit54tad@gmail.com>
2022-10-14 15:18:21 -0700

    now

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,25 +5,21 @@
  * @array: array to serch through
  * @size: size of array
  * @cmp: function used to compare
+ *
  * Return: first index for which cmp doesn't return 0, or -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 
 {
-int i, r;
-if (size > 0 && array && cmp)
+int i;
+if (size < 1 || array == NULL || cmp == NULL)
 
 {
 
 for (i = 0; i < size; i++)
-
-{
-r = cmp(array[i]);
-if (r)
-break;
 }
-if (i < size)
+if (cmp(array[i] ))
 return (i);
 }
 
