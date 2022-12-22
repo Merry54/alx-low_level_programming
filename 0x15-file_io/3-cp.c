f36332d61850d9a319d65438dd9094f022a5cc7a
merhawit <merhawit54tad@gmail.com>
2022-12-22 11:06:34 -0800

    three

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,113 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
+void error_file_from(char *, char *);
+void error_file_to(char *, char *);
+void error_close(int, char *);
 
 /**
- * main - copies the content of one file to another
- *  @argc: argument count
- * @argv: arguments passed
- *
- * Return: 1 on success, exit otherwise
+ * copy_file - copies a file to another
+ * @file_to: file to copy to
+ * @file_from: file to copy from
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int copy_file(char *file_to, char *file_from)
+{
+	int to, from, wr, err, re;
+	char *buf;
 
-{
-int src, dest, n_read = 1024, wrote, close_src, close_dest;		
-unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-char buffer[1024];
-if (argc != 3)
-{
-							
-dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
-exit(97);							
-}
-src = open(argv[1], O_RDONLY);
-check_IO_stat(src, -1, argv[1], 'O');
-dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-check_IO_stat(dest, -1, argv[2], 'W')
-while (n_read == 1024)
-{
-n_read = read(src, buffer, sizeof(buffer));
-				
-if (n_read == -1)
-check_IO_stat(-1, -1, argv[1], 'O');				
-wrote = write(dest, buffer, n_read);												
-if (wrote == -1)
-check_IO_stat(-1, -1, argv[2], 'W');
-																
-}	
-close_src = close(src);
-check_IO_stat(close_src, src, NULL, 'C');
-close_dest = close(dest);
-check_IO_stat(close_dest, dest, NULL, 'C');
-return (0);
+	buf = malloc(sizeof(char) * 1024);
+	if (!buf)
+		return (0);
 
+	from = open(file_from, O_RDONLY);
+	if (from == -1)
+		error_file_from(file_from, buf);
+
+	to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (to == -1)
+		error_file_to(file_to, buf);
+
+	re = read(from, buf, 1024);
+
+	do {
+		if (re == -1)
+			error_file_from(file_from, buf);
+		if (re == 0)
+			break;
+		wr = write(to, buf, re);
+		if (wr == -1)
+			error_file_to(file_to, buf);
+		re = read(from, buf, 1024);
+	} while (re > 0);
+
+	err = close(to);
+	if (err == -1)
+		error_close(to, buf);
+	err = close(from);
+	if (err == -1)
+		error_close(from, buf);
+
+	free(buf);
+	return (0);
 }
 
 /**
- * check_IO_stat - checks if a file can be opened or closed
- * @stat: file descriptor of the file to be opened
- * @filename: name of the file
- * @mode: closing or opening
- * @fd: file descriptor
- *
+ * error_close - error procedure when issue with closing
+ * @fd: integer where file is opened
+ * @buf: buffer to free
  * Return: void
  */
-
-void check_IO_stat(int stat, int fd, char *filename, char mode)
-
+void error_close(int fd, char *buf)
 {
-if (mode == 'C' && stat == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);	
-}
-else if (mode == 'O' && stat == -1)
-{								
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-exit(98);							
-}
-else if (mode == 'W' && stat == -1)
-drintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
+	free(buf);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
 
+/**
+ * error_file_from - error procedure when issue with file from
+ * @file_from: name of file from
+ * @buf: buffer to free
+ * Return: void
+ */
+void error_file_from(char *file_from, char *buf)
+{
+	free(buf);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+
+/**
+ * error_file_to - error procedure when file to cant be created
+ * @file_to: name of file to
+ * @buf: buffer to free
+ * Return: void
+ */
+void error_file_to(char *file_to, char *buf)
+{
+	free(buf);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
+
+/**
+ * main - main of our program
+ * @argc: number of arugments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_file(argv[2], argv[1]);
+	return (0);
 }
