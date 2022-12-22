18de830d6d27bf6364d2beacfe4c9e77be06b2f1
merhawit <merhawit54tad@gmail.com>
2022-12-22 10:52:12 -0800

    0x15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,56 +1,33 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strlen - counts string length
- * @str: string to be used
- * Return: length of the string
+ * append_text_to_file - appends text at end of file
+ * @filename: name of file
  *
- */
-
-int _strlen(char *str)
-
-{	
-int len = 0;
-while (str[len] != '\0')
-len++;
-return (len);
-
-}
-
-/**
- * append_text_to_file - appends text at the end of a file
- * @filename: name of the file
- * @text_content: content to be appended
- *
- * Return: 1 on success, -1 otherwise
+ * @text_content: content to append
+ * Return: 1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
-
 {
-int file, wrote;i
-if (filename == NULL)
-return (-1);
-file = open(filename, O_WRONLY | O_APPEND);
-if (file == -1)
-return (-1);
-if (text_content != NULL)
-{
-wrote =   write(file, text_content, _strlen(text_content));
-if (wrote == -1)						
-{										
-close(file);											
-return (-1);							
-}								
-close(file);											
-return (1);
+	int file, w, le;
 
-}
-else
-{												
-close(file);													
-return (1);
-}
-
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	if (text_content)	
+	{
+		for (le = 0; *(text_content + le);)
+			le++;
+		w = write(file, text_content, le);
+		if (w == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
