ac539109571a8943bfd21cbcdc3eaa13b443671d
merhawit <merhawit54tad@gmail.com>
2022-12-22 10:47:39 -0800

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,56 +1,40 @@
 #include "main.h"
-#include <stddef.h>
-
-/**
- *_strlen - counts string length
- * @str: string to be used
- *
- * Return: length of the string
- */
-
-int _strlen(char *str)
-
-{
-int len = 0;		
-while (str[len] != '\0')
-len++;
-return (len);
-
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * create_file - creates a file
- * @filename: name of the file
- * @text_content: content of the file to be created
- *
- * Return: 1 on success, -1 otherwise
+ * @filename: name of file
+ * @text_content: string with content of the file
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
+
 {
 
-int file, wrote;
-if (filename == NULL);
-return (-1);
-file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (file == -1)
-return (-1);
-if (text_content != NULL)
-{	
-wrote = write(file, text_content, _strlen(text_content));
-								
-if (wrote == -1
-{															
-close(file);							
-return (-1);	
-}							
-close(file);
-return (1);
-}
-else								
-{								
-close(file);
-return (1);
-}
+	int fd, rd, wr;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+
+	{
+		for (rd = 0; *(text_content + rd);)
+			rd++;
+		wr = write(fd, text_content, rd);
+		if (wr != rd)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 
 }
