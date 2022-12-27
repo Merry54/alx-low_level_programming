83d312a2df73daa06c776cc5b28b5b1c70dfcddb
merhawit <merhawit54tad@gmail.com>
2022-12-27 09:59:55 -0800

    zero

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -5,17 +5,12 @@
 /**
  * main - check the code
  * Return: Always 0.
- *
  */
-
 int main(int ac, char **av)
-
 {
 ssize_t n;
-
-if (ac != 2)
+if (ac !=2) 
 {
-
 dprintf(2, "Usage: %s filename\n", av[0]);
 exit(1);
 }
